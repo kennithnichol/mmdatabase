5d4b9553b9dc8381bc616bf97e43151d2a23b884
Kennith Nichol <kennith.nichol@toptal.com>
2021-08-26 20:04:29 -0700

    preparing for other controllers/views for the other models

---
+++ app/Http/Livewire/ComposerForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ComposerForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.composer-form');
+    }
+}
