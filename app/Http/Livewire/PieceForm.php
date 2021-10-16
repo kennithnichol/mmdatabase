ba7127958ee16ae1a8340748a95b6b268475cb67
Kennith Nichol <kennith.nichol@toptal.com>
2021-10-15 23:49:52 -0700

    add Piece form with support for modal addition.

--- app/Http/Livewire/PieceForm.php
+++ app/Http/Livewire/PieceForm.php
@@ -3,74 +3,175 @@
 namespace App\Http\Livewire;
 
 use App\Models\Composer;
+use App\Models\Movement;
 use App\Models\Piece;
 use Livewire\Component;
 
 class PieceForm extends Component
 {
-    public $designTemplate = 'bootstrap';
-    public $piece;
-    public $composer;
-    public $title;
-    public $year_published;
+	public $designTemplate = 'bootstrap';
+	public $piece;
+	public $composer;
+	public $title;
+	public $year_composed;
+	public $movements;
+	public $deletedMovements;
 
-    public $inModal;
+	public $inModal;
+	public $showModal = false;
+	public $isEditing = false;
+	public $movement;
+	public $editIndex;
 
-    protected $listeners = [
-        'pieceModalClosed' => 'modalClosed',
-    ];
 
-    protected $rules = [
-        'title' => 'required|unique:pieces,name',
-        'year_published' => 'numeric|min:4|nullable'
-    ];
+	protected $listeners = [
+		'pieceModalClosed' => 'modalClosed',
+	];
 
-    public function modalClosed()
-    {
-        $this->reset();
-        $this->hydrate();
-    }
+	public function rules(): array
+	{
+		if ($this->showModal) {
+			return [
+				'movement.order' => 'required|integer',
+				'movement.number' => 'required|integer',
+				'movement.title' => '',
+			];
+		}
+		return [
+			'title' => 'required',
+			'year_composed' => 'numeric|min:4|nullable',
+			'movements' => ''
+		];
+	}
 
-    public function hyrdate()
-    {
-        $this->resetErrorBag();
-        $this->resetValidation();
-    }
+	public function modalClosed()
+	{
+		$this->reset();
+		$this->hydrate();
+	}
 
-    public function mount(?Piece $piece, Composer $composer, bool $inModal = false)
-    {
-        $this->composer = $composer;
-        $this->inModal = $inModal;
-        $this->piece = $piece ?? new Piece();
+	public function mount(?Piece $piece, ?int $composer, bool $inModal = false)
+	{
+		$this->composer = $composer;
+		$this->inModal = $inModal;
+		$this->piece = $piece ?? new Piece();
+		$this->year_composed = null;
+		$this->movement = null;
 
-        if ($piece->id) {
-            $this->title = $piece->title;
-            $this->year_published = $piece->year_published;
-        }
-    }
+		$this->movements = collect();
+		$this->deletedMovements = collect();
 
-    public function save()
-    {
-        $this->validate();
+		if ($piece->id) {
+			$this->title = $piece->title;
+			$this->year_composed = $piece->year_composed;
+			$this->movements = $piece->movements;
+			$this->movements = $this->movements->sortBy('order');
+		}
+	}
 
-        $this->piece->title = $this->title;
-        $this->piece->year_published = $this->year_published;
-        $this->piece->save();
+	public function addMovement()
+	{
+		$this->movement = new Movement();
+		$this->movement['order'] = $this->movements->count() + 1;
+		$this->showModal = true;
+	}
 
-        $this->emit('pieceSaved', $this->piece->id);
+	public function editMovement($index)
+	{
+		$this->showModal = true;
+		$this->isEditing = true;
+		$this->editIndex = $index;
+		$this->movement = $this->movements->get($index);
+	}
 
-        if ($this->inModal)
-        {
-            $this->emit('changepiece', $this->piece->id);
-            $this->reset();
-            $this->inModal = true;
-            $this->piece = new Piece();
-            return;
-        }
+	public function removeMovement($index)
+	{
+		$this->deletedMovements->push($this->movements->get($index));
+		$this->movements->forget($index);
+	}
 
-        return $this->redirectRoute('home');
+	public function saveMovement()
+	{
+		$this->validate();
 
-    }
+		if ($this->isEditing) {
+			$this->movements[$this->editIndex] = $this->movement;
+		} else {
+			$this->movements->push($this->movement);
+		}
+
+		$this->emit('movementSaved');
+		$this->close();
+	}
+
+	public function hydrate()
+	{
+		$this->resetErrorBag();
+		$this->resetValidation();
+
+		$this->movements = $this->rehydrateMovements($this->movements);
+		$this->deletedMovements = $this->rehydrateMovements($this->deletedMovements);
+	}
+
+	protected function rehydrateMovements($movements)
+	{
+		$rehydrtatedMovements = collect();
+
+		foreach($movements as $movement) {
+			if ($movement instanceof Movement) {
+				$rehydrtatedMovements->push($movement);
+			} else {
+				if (isset($movement['id'])) {
+					$rehydrtatedMovements->push(Movement::find($movement['id']));
+				} else {
+					$rehydrtatedMovements->push(new Movement($movement));
+				}
+			}
+		}
+
+		return $rehydrtatedMovements;
+	}
+
+	public function close()
+	{
+		$this->showModal = false;
+		$this->isEditing = false;
+	}
+
+	public function save()
+	{
+		$this->validate();
+
+		$this->movements = $this->movements->map(function ($movement, $key) {
+			$movement->order = $key + 1;
+			return $movement;
+		});
+
+
+		$this->piece->title = $this->title;
+		$this->piece->year_composed = $this->year_composed;
+		$this->piece->composer_id = $this->composer;
+		$this->piece->save();
+
+		$this->piece->movements()->saveMany($this->movements->all());
+
+		$this->deletedMovements->each(function ($movement) {
+			$movement->delete();
+		});
+
+		$this->emit('pieceSaved', $this->piece->id);
+
+		if ($this->inModal) {
+			$this->emit('changepiece', $this->piece->id);
+			$this->reset();
+			$this->inModal = true;
+			$this->piece = new Piece();
+
+			return;
+		}
+
+		return $this->redirectRoute('home');
+	}
 
     public function render()
     {
--- resources/views/livewire/piece-form.blade.php
+++ resources/views/livewire/piece-form.blade.php
@@ -1,3 +1,94 @@
 <div>
-    {{ $composer}}
+	<form wire:submit.prevent="save" method="POST">
+		@csrf
+		<div class="row g-2">
+			<div class="col-12">
+				<label for="title" class="form-label">{{ __('Title') }}</label>
+				<input id="title" type="text" wire:model="title"
+					   class="form-control @error('title') is-invalid @enderror">
+				@error('title')
+				<span class="invalid-feedback" role="alert">{{ $message }}</span>
+				@enderror
+			</div>
+			<div class="col-12">
+				<label for="year_composed" class="form-label">{{ __('Year of composition') }}</label>
+				<input id="year_composed" type="tel" maxlength="4" wire:model="year_composed"
+					   class="form-control @error('year_composed') is-invalid @enderror">
+				@error('year_composed')
+				<span class="invalid-feedback" role="alert">{{ $message }}</span>
+				@enderror
+			</div>
+			<div class="col-12">
+				<h2>Movements</h2>
+				<table class="table">
+					<thead>
+					<tr>
+						<th scope="col" title="order #">#</th>
+						<th scope="col" title="Movement number text">Movement</th>
+						<th scope="col">Title</th>
+						<th></th>
+					</tr>
+					</thead>
+					<tbody wire:sortable="updateMovementOrder">
+					@php dump($movements) @endphp
+					@forelse($movements as $index => $movement)
+						<tr wire:sortable.item="{{ $movement->id }}" wire:key="movement-{{ $movement->order }}">
+							<th wire:sortable.handle scope="row">{{ $movement['order'] }}</th>
+							<td>{{ $movement->number }}</td>
+							<td>{{ $movement->title }}</td>
+							<td>
+								<button wire:click.prevent="editMovement({{ $index }})"
+										data-bs-toggle="modal" data-bs-target="#movement-modal"
+										class="btn btn-outline-primary">Edit
+								</button>
+								<button wire:click.prevent="removeMovement({{ $index }})"
+										class="btn btn-danger">Edit
+								</button>
+							</td>
+						</tr>
+					@empty
+						<tr>
+							<td colspan="4" class="text-center">No movements added</td>
+						</tr>
+					@endforelse
+					</tbody>
+				</table>
+				<button wire:click.prevent="addMovement" type="button" data-bs-toggle="modal"
+						data-bs-target="#movement-modal" class="btn btn-outline-primary">Add Movement
+				</button>
+				<button type="submit" class="btn btn-primary">Save</button>
+
+				@if ($errors && !$this->showModal)
+					@foreach ($errors->all() as $error)
+						<span>{{ $error }}</span>
+					@endforeach
+				@endif
+			</div>
+		</div>
+	</form>
+
+	<div wire:ignore.self id="movement-modal" class="modal fade" role="dialog">
+		<div class="modal-dialog modal-dialog-centered" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5>Add movement</h5>
+					<button wire:click.prevent="closeMovement" type="button" class="btn-close"
+							data-bs-dismiss="modal"></button>
+				</div>
+				<div class="modal-body form-row">
+					-Movement Form-
+					<form>
+						<button type="submit" class="btn btn-primary">Save</button>
+						<button wire:click.prevent="close" type="button" class="btn btn-outline-secondary"
+								data-bs-dismiss="modal">Cancel
+						</button>
+						<input type="hidden" wire:model="movement.order"
+							   value="{{ $movement->order ?? $movements->count() + 1 }}"/>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	{{ $composer }}
+	{{ $inModal }}
 </div>
